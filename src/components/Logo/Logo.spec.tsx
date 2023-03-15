eff6874110ec43c725ffce4d29d7a20b1a1f0f43
felixstenian <felixstenian@gmail.com>
2023-03-15 02:01:48 -0300

    feat: add prop with TDD

--- src/components/Logo/Logo.spec.tsx
+++ src/components/Logo/Logo.spec.tsx
@@ -1,13 +1,32 @@
 import { render, screen } from '@testing-library/react'
 
+import { renderWithTheme } from 'utils'
+
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('1. should render the reading', () => {
-    const { container } = render(<Logo />)
-
-    expect(screen.getByRole('heading', { name: Logo })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
-  })
+  it('1. should render a white label by default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  }),
+    it('2. should render a black label when color is passed', () => {
+      renderWithTheme(<Logo color="black" />)
+      expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+        color: '#030517'
+      })
+    }),
+    it('3. should render a normal logo when size is default', () => {
+      renderWithTheme(<Logo />)
+      expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+        width: '11rem'
+      })
+    }),
+    it('4. should render bigger logo', () => {
+      renderWithTheme(<Logo size="large" />)
+      expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+        width: '20rem'
+      })
+    })
 })
