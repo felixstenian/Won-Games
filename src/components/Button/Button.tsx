7575114c18b1e74e0f1ed3fd12fcdaec47b37484
felixstenian <felixstenian@gmail.com>
2023-03-18 00:43:18 -0300

    refactor: add prop fullWidth in button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,10 +3,15 @@ import * as Style from './styles'
 export type ButtonProps = {
   children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
+  fullWidth?: boolean
 }
 
-const Button = ({ children, size = 'medium' }: ButtonProps) => (
-  <Style.Wrapper size={size}>
+const Button = ({
+  children,
+  size = 'medium',
+  fullWidth = false
+}: ButtonProps) => (
+  <Style.Wrapper size={size} fullWidth={fullWidth}>
     {!!children && <span>{children}</span>}
   </Style.Wrapper>
 )
