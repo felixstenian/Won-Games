bea3e2fcf91bb31445538caf8c8684a3e0173183
felixstenian <felixstenian@gmail.com>
2023-04-26 23:20:44 -0300

    chore: update storybook version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,15 @@
 module.exports = {
   "stories": ["../src/**/*.stories.mdx", "../src/components/**/stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ['../public'],
   "addons": ["@storybook/addon-essentials"],
   "framework": {
     name: "@storybook/nextjs",
     options: {}
   },
-  webpackFinal: config => {
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  },
+  webpackFinal: async (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`);
     return config;
   }
